62a9ec4ad99c9f06689a3559c2a347f003c5a5ae
Yufeng <guyufeng@zju.edu.cn>
2020-08-17 18:44:49 +0800

    Fish

--- hello.py
+++ hello.py
@@ -1,13 +1,17 @@
-def hello():
+hello():
     print("Hello World!")
 
 def cat():
     print("Cat!")
 
-def dog();
+def dog():
     print("Dog!")
+
+def fish():
+    print("Fish!")
 
 if __name__ == '__main__':
     hello()
     cat()
     dog()
+    fish()
